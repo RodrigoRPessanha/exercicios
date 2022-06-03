f952a1999f5efe30137c015b6358901858462573
RodrigoRPessanha <rodrigorospe@outlook.com>
2022-06-03 14:05:34 -0300

    javascript 1week

--- Exercicio15/main.js
+++ Exercicio15/main.js
@@ -1,10 +1,13 @@
 function fatorial(event){
     event.preventDefault();
-    let numero = document.getElementById("fatorial").value;
+    numero = document.getElementById("fatorial").value;
+    let fatorial = 0;
     let contador;
-    for(contador = 1; contador <= num; contador++) {
-        numero = numero*contador;
+    for(contador = 1; contador <= numero; contador++) {
+        fatorial = numero*contador;
     }
-    
+    alert(fatorial);
+    console.log(fatorial)
 }
+
 document.getElementById("form").addEventListener('submit', fatorial);
